d0ec3541ea0559f3025e3ae3e3b95e9c34799e3c
Clément <clement.caloin@gmail.com>
2020-05-20 20:25:16 +0200

    Structure change
    Removed the nested object to have one single json object

--- dicewareList.py
+++ dicewareList.py
@@ -4,19 +4,15 @@ import os
 inputPath = os.path.join(os.getcwd(),'Input')
 
 for file in os.listdir(inputPath):
-    dicewareList = []
+    dicewareDict = {}
     with open(os.path.join(inputPath, file), 'r', encoding='utf-8') as content:
         for line in content:
             lineElements = line.split()
-            lineDictionnary = {
-                'dices': lineElements[0],
-                'value': lineElements[1]
-            }
-            dicewareList.append(lineDictionnary)
+            dicewareDict[lineElements[0]] = lineElements[1]
     try:
         outputName = f'{os.path.splitext(file)[0]}.json'
         with open(f'./Output/{outputName}','w') as outfile:
-            json.dump(dicewareList,outfile)
+            json.dump(dicewareDict,outfile)
         print(f'{outputName} created')
     except error:
         print(error)
